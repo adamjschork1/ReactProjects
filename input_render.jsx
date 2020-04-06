59f82e8cdf51c9a45c2ecd8a438b989a965e152c
Adam Schork <adamjschork@gmail.com>
2020-04-05 19:34:41 -0700

    Create input_render.jsx
    Renders text that is input and submitted through the text field. Inital and fully functional React component.

---
+++ input_render.jsx
@@ -0,0 +1,36 @@
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+  handleChange (event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit (event) {
+    event.preventDefault()
+    this.setState({
+      submit: this.state.input
+    });
+  }
+  
+  render () {
+  return(
+  <div>
+      <form onSubmit={this.handleSubmit}>
+        <input value={this.state.input} onChange={this.handleChange}></input>
+        <button type='submit'>Submit</button>
+      </form>
+      <p>{this.state.submit}</p>
+  </div>
+  );
+  }
+};
+
+ReactDOM.render(<MyForm/>, document.getElementById('view'));
