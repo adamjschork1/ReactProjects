ccbf8e354df94122f3ee43e3ed9a63a925e0150c
Adam Schork <adamjschork@gmail.com>
2020-05-11 18:42:51 -0700

    Create redux_store.js
    Initial commit for extracting state logic to Redux

---
+++ message_app/redux_store.js
@@ -0,0 +1,19 @@
+const ADD = 'ADD';
+
+const addMessage = message => {
+  return({
+    type: ADD,
+    message
+  });
+}
+
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [...state, action.message];
+    default:
+      return state;
+  }
+}
+
+const store = Redux.createStore(messageReducer);
