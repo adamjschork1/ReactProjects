d63babfb379fc46a0177fc62600626b2f19ad183
Adam Schork <adamjschork@gmail.com>
2020-05-11 17:40:48 -0700

    Update message_app.js
    Added handleChange and submitChange functionality to store messages in an array and render them on screen

--- message_app/message_app.js
+++ message_app/message_app.js
@@ -5,12 +5,28 @@ class DisplayMessages extends React.Component {
       input: '',
       messages: []
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
   };
-  
+  handleChange(e) {
+    this.setState({
+      input: e.target.value
+    })
+  }
+  submitMessage() {
+    this.setState({
+      messages: [...this.state.messages, this.state.input],
+      input: ''
+    })
+  }
   render () {
+  const messageList = this.state.messages.map(i => <li>{i}</li>);
   return(
   <div>
-      <p>Nothing yet</p>
+    <h2>Type in a new Message:</h2>
+    <input value={this.state.input} onChange={this.handleChange}/>
+    <button onClick={this.submitMessage}>Submit</button>
+    <ul>{messageList}</ul>
   </div>
   );
   }
